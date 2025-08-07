4c4866abdf95e1a626ae8d56e823dcaea261a9a3
josephus <otejerejosephus@gmail.com>
2025-08-08 00:55:58 +0100

    added poke api render

---
+++ src/components/PokeCard.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react"
+import { getFullPokedexNumber, getPokedexNumber } from "../utils"
+
+type pokeCardProp = {
+    selectedPokemon: number
+}
+
+export function PokeCard(props: pokeCardProp) {
+    const { selectedPokemon } = props
+    const [ data, setData ] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const { name, height, abilities, stats, types, moves, sprites} = (data as any) || {}
+
+    useEffect(() => {
+        // if loading, exit logic
+        if (loading || !localStorage) { return }
+
+        // check if the selected pokemon information is availale in the cache
+        // 1. define the cache
+        let cache: Record<string, any> = {}
+        if (localStorage.getItem('pokedex')) {
+            cache = JSON.parse(localStorage.getItem('pokedex') as string)
+        }
+
+        // 2. check if the selected pokemon is in the cache otherwise fetch from api
+
+        if (selectedPokemon in cache) {
+            //read from cache
+            setData(cache[selectedPokemon])
+            return
+        }
+
+        // we passed all the cache stuff to no avail and now we need to fetch from api
+
+        async function fetchPokemonData() {
+            try {
+                const baseUrl = 'https://pokeapi.co/api/v2/'
+                const suffix = 'pokemon/' + getPokedexNumber(selectedPokemon)
+                const finalUrl = baseUrl + suffix
+                const res = await fetch(finalUrl)
+                const pokemonData = await await res.json()
+                setData(pokemonData)
+                console.log(pokemonData)
+
+                cache[selectedPokemon] = pokemonData
+                localStorage.setItem('pokedex', JSON.stringify(cache))
+            } catch (error: any) {
+                console.log(error.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchPokemonData()
+
+        // if we fetch from api, save information to the cache for next time
+
+    }, [selectedPokemon])
+
+    if (loading || !data) {
+        return <div> 
+            <h4>Loading...</h4>
+        </div>
+    }
+
+    return(
+        <div className="poke-card">
+            <div> 
+                <h4>#{getFullPokedexNumber(selectedPokemon)}</h4>
+                <h2>{name}</h2>
+            </div>
+            
+            
+        </div>
+    )
+}
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,25 @@
+import { first151Pokemon, getPokedexNumber } from "../utils"
+
+type sideNavProps = {
+    selectedPokemon: number
+    setSelectedPokemon: any
+}
+
+export function SideNav(props: sideNavProps) {
+    return (
+        <nav>
+            <div className="header">
+                <h1 className="text-gradient">Pokedex</h1>
+            </div>
+            <input />
+            {first151Pokemon.map((pokemon, pokemonIndex) => {
+                return (
+                    <button key={pokemonIndex} className={'nav-card '}>
+                        <p>{getPokedexNumber(pokemonIndex)}</p>
+                        <p>{pokemon}</p>
+                    </button>
+                )
+            })}
+        </nav>
+    )
+}
